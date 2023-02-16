545de7305d9d3c0d81c7886e93c890dfc0cb7cbe
jjkim <kljj0650@naver.com>
2023-02-16 14:56:44 +0900

    오타 수정 및 파일 정리

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,11 +1,11 @@
 DROP USER IF EXISTS 'monty'@'localhost';
 DROP DATABASE IF EXISTS monty_hall;
 
-CREATE DATABASE monty_hall_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE monty_hall DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'monty'@'localhost' IDENTIFIED BY '1q2w3e4r';
-GRANT ALL On monty_hall_db.* TO 'monty'@'localhost';
+GRANT ALL On monty_hall.* TO 'monty'@'localhost';
 
-USE monty_hall_db;
+USE monty_hall;
 
 CREATE TABLE variables (
     seq     INT NOT NULL AUTO_INCREMENT,
