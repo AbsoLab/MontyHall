916a0ae4380ff6fe36000aa3652581f89954e52f
jjkim <kljj0650@naver.com>
2023-02-16 16:44:57 +0900

    서버 끝

--- server/db/service.js
+++ server/db/service.js
@@ -10,6 +10,8 @@ const conn = mysql.createConnection({
 });
 conn.connect();
 
+const KEY = ['CHANGE_WIN', 'CHANGE_LOSE', 'UNCHANGE_WIN', 'UNCHANGE_LOSE'];
+
 const getAllData = (callback) => {
     conn.query(`SELECT * FROM variables`, (err, rows, fields) => {
         if (err) throw err;
@@ -17,6 +19,26 @@ const getAllData = (callback) => {
     });
 }
 
+const updateChangeWin = () => {
+    conn.query(`UPDATE variables SET val = val + 1 WHERE name='${KEY[0]}'`);
+}
+
+const updateChangeLose = () => {
+    conn.query(`UPDATE variables SET val = val + 1 WHERE name='${KEY[1]}'`);
+}
+
+const updateUnChangeWin = () => {
+    conn.query(`UPDATE variables SET val = val + 1 WHERE name='${KEY[2]}'`);
+}
+
+const updateUnChangeLose = () => {
+    conn.query(`UPDATE variables SET val = val + 1 WHERE name='${KEY[3]}'`);
+}
+
 module.exports = {
-    getAllData
+    getAllData,
+    updateChangeWin,
+    updateChangeLose,
+    updateUnChangeWin,
+    updateUnChangeLose
 }
