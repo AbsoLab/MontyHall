8ff8223119fc68ee3cc631da72613817f7b72b29
jjkim <kljj0650@naver.com>
2023-03-06 11:37:58 +0900

    문 컴포넌트 제작

---
+++ monty/src/components/Door.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let state = 0;
+
+    const imgs = ['1.png', '2.png', '3.png', '4.png'];
+
+    let path;
+    $: path = imgs[state];
+
+    const handleMouseEnter = () => {
+        if (state == 0) {
+            path = imgs[1];
+        }
+    }
+    const handleMouseLeave = () => {
+        if (state == 0) {
+            path = imgs[0];
+        }
+    }
+</script>
+
+<img src="{path}" alt="" on:mouseenter="{handleMouseEnter}" on:mouseleave="{handleMouseLeave}">
+
+<style>
+    img:hover { cursor: pointer; }
+</style>
